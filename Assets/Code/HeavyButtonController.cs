188769d2b3873ff554729a283c0b307cbbfa7b20
Kevin Michel <kmichel.info@gmail.com>
2014-12-07 11:40:35 +0100

    Add button animation

--- Assets/Code/HeavyButtonController.cs
+++ Assets/Code/HeavyButtonController.cs
@@ -4,8 +4,17 @@ using System.Collections;
 public class HeavyButtonController : MonoBehaviour {
 
 	public ScenarioController scenario;
+	public Transform model;
 
 	public float minimumVelocity = 38.0f;
+
+	void OnEnable() {
+		model.localPosition = new Vector3(0, -0.5f, 0);
+	}
+
+	void Update() {
+		model.localPosition = new Vector3(0, Mathf.Min(model.localPosition.y + 4 * Time.deltaTime, 0.25f), 0);
+	}
 
 	void OnTriggerEnter(Collider other) {
 		var playerController = other.GetComponent<PlayerController>();
