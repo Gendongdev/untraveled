ac4b2ace71e12712d92f08ea11f9903d394b8e80
Kevin Michel <kmichel.info@gmail.com>
2014-12-07 16:15:45 +0100

    Visual tweaks

--- Assets/Code/StarController.cs
+++ Assets/Code/StarController.cs
@@ -6,11 +6,16 @@ public class StarController : MonoBehaviour {
 	[System.NonSerialized]
 	Material material;
 
+	[System.NonSerialized]
+	Vector3 baseScale;
+
 	void Start () {
 		material = GetComponent<MeshRenderer>().material;
+		baseScale = transform.localScale;
 	}
 	
 	void Update () {
 		material.color = new Color(1, 1, 1, 0.2f + 0.3f * Mathf.PerlinNoise(transform.position.x * transform.position.y, Time.time * 3.0f));
+		transform.localScale = baseScale * (0.7f + 0.6f * Mathf.PerlinNoise(transform.position.x * transform.position.y, Time.time));
 	}
 }
