3e9ea793965c613705af90108d73d1dec7cdb7f2
Kevin Michel <kmichel.info@gmail.com>
2014-12-07 16:31:06 +0100

    Enforce visible area for all screen ratio

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	[Range(1, 180)]
+	public float verticalFov = 38.0f;
+
+	[Range(0.1f, 10)]
+	public float targetRatio = 1.6f;
+
+	void Update () {
+		var screenRatio = ((float)Screen.width) / Screen.height;
+		if (screenRatio < targetRatio) {
+			camera.fieldOfView = verticalFov / screenRatio * targetRatio;
+		} else {
+			camera.fieldOfView = verticalFov;
+		}
+	}
+}
