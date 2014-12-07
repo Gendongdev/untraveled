8100b9175ed1a0590626db67793164f80f85b560
Kevin Michel <kmichel.info@gmail.com>
2014-12-07 15:35:37 +0100

    Add obstacle models, textures and item glow

---
+++ Assets/Code/StarController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour {
+
+	[System.NonSerialized]
+	Material material;
+
+	void Start () {
+		material = GetComponent<MeshRenderer>().material;
+	}
+	
+	void Update () {
+		material.color = new Color(1, 1, 1, 0.2f + 0.3f * Mathf.PerlinNoise(transform.position.x * transform.position.y, Time.time * 3.0f));
+	}
+}
